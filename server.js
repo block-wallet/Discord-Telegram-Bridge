e6bce4adb98f4889d2c4f63614ebff7d3bfd181b
Julian Ariel Martinez <julymartinez94@gmail.com>
2022-10-03 17:40:51 -0300

    added dotenv. removed Discord to Telegram code.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import { discordClient, discordWebhookClient } from './backends/discord.js';
+import './env.js';
+import { discordWebhookClient } from './backends/discord.js';
 import { telegram, telegramGetFileURL, telegramGetProfilePic } from './backends/telegram.js';
 
 import { enable_heroku } from './utils/heroku.js';
@@ -11,54 +12,13 @@ if ("HEROKU_DYNO_URL" in process.env) {
 // import env variables
 const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 const DISCORD_CHANNEL_ID = process.env.DISCORD_CHANNEL_ID;
-const DISCORD_FORWARD_BOT = (process.env.DISCORD_FORWARD_BOT === 'true')
 
 console.log("Telegram chat id: " + TELEGRAM_CHAT_ID);
 console.log("Discord channel id: " + DISCORD_CHANNEL_ID);
 
-// Discord -> Telegram handler
-discordClient.on("message", message => {
-
-	// the program currently check if the message's from a bot to check for duplicates.
-	// This isn't the best method but it's good enough.
-	// A webhook counts as a bot in the discord api, don't ask me why.
-	// Ignore messages from bots if DISCORD_FORWARD_BOT is 'false'
-	if (message.channel.id !== DISCORD_CHANNEL_ID || (message.author.bot && !DISCORD_FORWARD_BOT)) {
-		return;
-	}
-
-	let mentioned_usernames = []
-	for (let mention of message.mentions.users) {
-		mentioned_usernames.push("@" + mention[1].username);
-	}
-	var attachmentUrls = []
-	for (let attachment of message.attachments) {
-		attachmentUrls.push(attachment[1].url);
-	}
-
-	// attachmentUrls is empty when there are no attachments so we can be just lazy
-	var finalMessageContent = message.content.replace(/<@.*>/gi, '');
-	// convert bold text for telegram markdown
-	finalMessageContent = finalMessageContent.replace(/\*\*/g, '*');
-
-	var text = `*\[DISCORD\] ${message.author.username} (${message.author.username}#${message.author.discriminator}):*\n`;
-	text += finalMessageContent
-	text += ` ${attachmentUrls.join(' ')}`;
-	text += mentioned_usernames.join(" ");
-
-	try {
-		telegram.sendMessage(TELEGRAM_CHAT_ID, text, {parse_mode: 'markdown'});
-	}
-	catch(err) {
-		console.log(err.message);
-		return;
-	}
-});
-
 // Telegram -> Discord handler
 telegram.on("message", async (message) => {
 
-	// console.log(message)
 	if (message.chat.id != TELEGRAM_CHAT_ID) {
 		return;
 	}
@@ -122,17 +82,17 @@ telegram.on("message", async (message) => {
 
 			if (fileUrl != "") {
 				discordWebhookClient.send(text, {
-					username: username,	avatarURL: profileUrl, files: [fileUrl]
+					username: username, avatarURL: profileUrl, files: [fileUrl]
 				});
 			}
 		}
 		if (!fileId || fileUrl == "") {
 			await discordWebhookClient.send(text, {
-				username: username,	avatarURL: profileUrl
+				username: username, avatarURL: profileUrl
 			});
 		}
 	}
-	catch(err) {
+	catch (err) {
 		console.log(err.message);
 		return;
 	}
@@ -146,15 +106,15 @@ function convert_text_telegram_to_discord(text, entities) {
 	var section_end;
 	var section_start;
 
-	entities.forEach(({type, offset, length, url}) => {
+	entities.forEach(({ type, offset, length, url }) => {
 		convert = true;
 		if (type == 'bold') {
 			start_format = '\*\*';
 			end_format = '\*\*';
-		} else if(type == 'italic') {
+		} else if (type == 'italic') {
 			start_format = '\_';
 			end_format = '\_';
-		} else if(type == 'text_link') {
+		} else if (type == 'text_link') {
 			start_format = '\*\*';
 			end_format = '\*\* (<' + url + '>)';
 		} else {
